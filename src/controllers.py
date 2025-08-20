f70f7ed8aa4b25aedf81deb10365bc0404c3a115
Fhil Joshua <husphil200@gmail.com>
2025-08-21 00:26:12 +0800

    trying to implement the scheduler of popping up of logger view

--- src/controllers.py
+++ src/controllers.py
@@ -1,4 +1,5 @@
-from math import exp
+import tkinter as tk
+from tkinter import messagebox
 from src.views import BaseView, LoggerView, SettingsView
 
 
@@ -6,7 +7,7 @@ class Controller:
 
     def __init__(self, model):
         self.model = model
-        self.root = None
+        self.root: tk.Tk = None
 
         self.views = {}
         self.current_view: BaseView = None
@@ -19,17 +20,38 @@ class Controller:
 
         self.show_logger_view()
 
+    def handle_close_request(self):
+        """
+        Handles the WM_DELETE_WINDOW protocol.
+        Prevents closing if the LoggerView is active.
+        """
+        if isinstance(self.current_view, LoggerView):
+            messagebox.showinfo(
+                "Cannot Close",
+                "You cannot close the application from the Logger View. Please navigate to Settings to close.",
+            )
+        else:
+            # Allow closing for all other views
+            self.root.destroy()
+
     def show_logger_view(self):
         self._hide_current_view()
         self.current_view = self.views["logger"]
         self.current_view.pack(expand=True, fill="both")
+        self.root.overrideredirect(True)
+        self.root.attributes("-topmost", True)
 
     def show_settings_view(self):
         self._hide_current_view()
         self.current_view = self.views["settings"]
         self.current_view.pack(expand=True, fill="both")
+        
 
     def _hide_current_view(self):
         """A helper method to hide the currently visible view."""
         if self.current_view:
+            self.root.overrideredirect(False)
+            self.root.attributes("-topmost", False)
             self.current_view.pack_forget()
+
+    
