9277f189626f071becfd1ebb1e2eaf3202380a00
Fhil Joshua <husphil200@gmail.com>
2025-08-20 23:17:12 +0800

    created the base view for the views: loggerview, settingsview

--- src/views.py
+++ src/views.py
@@ -1,1 +1,20 @@
+import tkinter as tk
 
+
+class BaseView(tk.Frame):
+    """Base class for all views in the application."""
+
+    def __init__(self, parent, controller):
+        super().__init__(parent)
+        self.controller = controller
+
+
+class LoggerView(BaseView):
+    def __init__(self, parent, controller):
+        super().__init__(parent, controller)
+
+
+class ConfigView(tk.Frame):
+    def __init__(self, parent, controller):
+        super().__init__(parent, controller)
+        self.controller = controller
